91732f6447062560a8c90fe8b4e946784fca81a9
Adil Imran <devadem1370@gmail.com>
2025-08-26 07:19:43 -0700

    create filter component

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -8,7 +8,7 @@ const Homepage = () => {
     <div className="relative aspect-[3/1] mb-12">
       <Image src="/featured.png" alt='Featured Product' fill/>
     </div>
-    <ProductList/>
+    <ProductList />
    </div>
 
   )
